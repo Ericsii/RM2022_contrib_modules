ac0ec630d3487a86130cfe988b57f256e6efc893
冯云龙 <ericc_feng@qq.com>
2022-04-23 14:52:42 +0000

    fix(rm_trajectory): 修改GimbalTransform的角度顺序

--- rm_trajectory/src/ballistic_trajectory/trajectory_gimbal.cpp
+++ rm_trajectory/src/ballistic_trajectory/trajectory_gimbal.cpp
@@ -6,17 +6,17 @@ namespace rm_trajectory
 bool TrajectoryGimbal::solve(double x, double y, double z, double & pitch, double & yaw)
 {
   if (!solver_) {
-    pitch = -atan2(z, x);
+    pitch = atan2(sqrt((y * y) + (x * x)), z);
   } else {
     double angle;
-    if (solver_->solve(z, x, angle)) {
-      pitch = -angle;
+    if (solver_->solve(sqrt((y * y) + (x * x)), z, angle)) {
+      pitch = angle;
     } else {
       error_message_ = solver_->error_message();
       return false;
     }
   }
-  yaw = atan2(y, x);
+  yaw = atan2(x, y);
   return true;
 }
 
