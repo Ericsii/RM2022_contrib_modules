20870f88e2c0c5a49a439640d2ca479c07fc5d5f
冯云龙 <ericc_feng@qq.com>
2022-04-27 14:11:14 +0800

    feat(rm_util): 加入pid控制器

---
+++ rm_util/include/rm_util/pid.hpp
@@ -0,0 +1,38 @@
+#ifndef RM_UTIL__PID_HPP_
+#define RM_UTIL__PID_HPP_
+
+namespace rm_util
+{
+    class PID
+    {
+    public:
+        PID(float kp, float ki, float kd, float dt, float min_output = 0, float max_output = 0);
+        ~PID();
+
+        void set_kp(float kp);
+        void set_ki(float ki);
+        void set_kd(float kd);
+        void set_dt(float dt);
+
+        float get_kp();
+        float get_ki();
+        float get_kd();
+        float get_dt();
+
+        float calc(float target, float current, float dt = 0.0f);
+    
+    private:
+        float kp_;
+        float ki_;
+        float kd_;
+        float dt_;
+
+        float last_error_;
+        float integral_;
+
+        float limit_min_, limit_max_;
+    };
+} // namespace rm_util
+
+
+#endif // RM_UTIL__PID_HPP_
