e520abb015904984e3719e07a718a37d2b9fc88f
文康 <1063760288@qq.com>
2022-05-03 20:23:08 +0800

    Accept Merge Request #2392: (fix/contrib_modules -> master) Merge Request: fix(rm_util):修改quat2euler四元数转欧拉角函数实现
    Created By: @文康
    Reviewed By: @冯云龙 @周备 @和永盟
    Approved By: @冯云龙 @和永盟 @周备 
    Accepted By: @文康
    URL: https://scurm.coding.net/p/2022-vision/d/contrib_modules/git/merge/2392

--- rm_trajectory/src/ballistic_trajectory/trajectory_gimbal.cpp
+++ rm_trajectory/src/ballistic_trajectory/trajectory_gimbal.cpp
@@ -3,21 +3,27 @@
 namespace rm_trajectory
 {
 
-bool TrajectoryGimbal::solve(double x, double y, double z, double & pitch, double & yaw)
-{
-  if (!solver_) {
-    pitch = atan2(sqrt((y * y) + (x * x)), z);
-  } else {
-    double angle;
-    if (solver_->solve(sqrt((y * y) + (x * x)), z, angle)) {
-      pitch = angle;
-    } else {
-      error_message_ = solver_->error_message();
-      return false;
+  bool TrajectoryGimbal::solve(double x, double y, double z, double &pitch, double &yaw)
+  {
+    if (!solver_)
+    {
+      pitch = atan2(sqrt((y * y) + (x * x)), z);
     }
+    else
+    {
+      double angle;
+      if (solver_->solve(sqrt((y * y) + (x * x)), z, angle))
+      {
+        pitch = angle;
+      }
+      else
+      {
+        error_message_ = solver_->error_message();
+        return false;
+      }
+    }
+    yaw = atan2(x, y);
+    return true;
   }
-  yaw = atan2(x, y);
-  return true;
-}
 
 }
